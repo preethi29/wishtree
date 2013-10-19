8653654931a4f25e8d789f5d64e84296fd3286d2
sajani <deepasaj@thoughtworks.com>
2013-10-19 16:38:37 +0530

    <Sajani>Created the initial controller, view for wish

---
+++ app/controllers/wish_controller.rb
@@ -0,0 +1,5 @@
+class WishController < ApplicationController
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Wishtree::Application.routes.draw do
+  get "wish/:id" => 'wish#show'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
