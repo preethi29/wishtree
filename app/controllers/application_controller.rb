8afaafbd07fd2cecf07970d14f99bb990eb5b96a
Preethi <preethis@thoughtworks.com>
2013-10-19 18:13:40 +0530

    [Preethi] added signup and sign in links in home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_devise_params, if: :devise_controller?
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :name, :contact_no)
+    end
+  end
   protect_from_forgery with: :exception
 end
