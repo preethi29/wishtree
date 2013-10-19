3e92007479a7b8c120cb58ba54188d2fcdd7ecd6
Preethi <preethis@thoughtworks.com>
2013-10-19 09:21:53 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wishtree::Application.config.secret_key_base = '2d3ca96a63126543ca463ddc6c3c940e8f433a7e626be85f8989f6eb416a81b9858b151dcab92da0084b2b5ca2a7636590bb83e4bb1c8eade419d8aa688ec4e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wishtree::Application.config.session_store :cookie_store, key: '_wishtree_session'
