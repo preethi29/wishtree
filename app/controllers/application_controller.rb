a74ebdc53e0442c2e7bfa092e36099c248275a90
sajani <deepasaj@thoughtworks.com>
2013-10-20 16:36:44 +0530

    <Sajani>Enabled proper redirection for sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
     end
   end
   protect_from_forgery with: :exception
+  after_filter :store_location
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless (request.fullpath =~ /\/users\/sign_in/ || request.fullpath =~ /\/users\/sign_out/ || request.fullpath =~ /\/users\/sign_up/ || request.fullpath =~ /\/users\/edit/ || request.fullpath =~ /\/ajax_utilities/ || request.fullpath =~ /\/assets/)
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
+  end
 end
