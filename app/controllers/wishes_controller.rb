74cfaa0e38affb2eb4dd1b4e99901a024b1fc885
Preethi <preethis@thoughtworks.com>
2013-10-20 18:11:54 +0530

    [Preethi/GS] Adding create new wish functionality

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,8 +1,24 @@
 class WishesController < ApplicationController
   def show
     @wish=Wish.find(params[:id])
+  end
+
+  def new
+    @wish = Wish.new
+  end
+
+  def create
+    params.require(:wish).permit!
+    @wish=Wish.create(params[:wish])
+    needy_params= {name: params[:name], age: params[:age], country: params[:country]}
+    @needy=Needy.create(needy_params)
+    if (@wish.save && @needy.save)
+      redirect_to root_path
+    else
+      render 'new'
     end
-  
+  end
+
   def index
     session[:guest_user_id]= nil
     @wishes = Wish.all
